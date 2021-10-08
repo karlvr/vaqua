67716017e252f28fad81d3e0d36239fb7e157b9e
Karl von Randow <karl@xk72.com>
2021-10-09 12:39:46 +1300

    Remove debug output when modal sheet session terminates

--- libvaqua/AquaNativeSupport.m
+++ libvaqua/AquaNativeSupport.m
@@ -2101,7 +2101,7 @@ JNIEXPORT jint JNICALL Java_org_violetlib_aqua_AquaSheetSupport_nativeDisplayAsS
     NSWindow *no = (NSWindow *) owner_wptr;
 
     runOnMainThread(^() {
-        [no beginSheet:w completionHandler:^(NSModalResponse r){NSLog(@"Modal sheet session terminated");}];
+        [no beginSheet:w completionHandler:nil];
     });
     result = 0;
 
