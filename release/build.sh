7397f18e5111f0f89188b50996dc850da89671d5
Karl von Randow <karl@xk72.com>
2022-06-21 23:03:13 +1200

    build: include native libaries

--- release/build.sh
+++ release/build.sh
@@ -5,3 +5,5 @@ cp dist/VAqua.jar ~/Development/charles/repo/com/xk72/violetlib/VAqua/$version/V
 cp dist/VAqua.jar ~/.m2/repository/com/xk72/violetlib/VAqua/$version/VAqua-$version.jar
 (cd ../src && jar cf ../release/VAqua-$version-sources.jar .)
 cp VAqua-$version-sources.jar ~/.m2/repository/com/xk72/violetlib/VAqua/$version/
+codesign out/jni/*.dylib out/uber-classes/*.dylib -s "Developer ID Application: XK72 Limited"
+rsync -a out/jni/*.dylib out/uber-classes/*.dylib ~/Development/charles/app/assembly/src/main/assembly/macos/lib
